f5fdd4146c7cf17617c001a44a6810e2c4956f2f
damian0604 <d.c.trilling@uva.nl>
2016-08-03 23:07:23 +0200

    added some processers

--- processing/basic_text_processing.py
+++ processing/basic_text_processing.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 from core.processor_class import Processer
 from core.basic_utils import dotkeys
 import logging
 import re
+from pattern.nl import parse
+# TODO I now use pattern.nl as it was basically copy/paste from the earlier version
+# TODO we might want to port this to Alpino, if we use Alpino anyway
+from sys import maxunicode
+import unicodedata
 
 logger = logging.getLogger(__name__)
 
@@ -24,3 +30,31 @@ class split_texts(Processer):
             [doc.extend(splittable.split(splitter)) for splittable in doc]
         return doc
             
+class njr(Processer):
+    '''Keeps only nouns (N), adjectives (J), and adverbs (R) (N, J, R are Penn Treebank tags)'''
+
+    def process(self, document_field):
+        '''everything except nouns, adjectives, and adverbs was removed'''
+        doc = ""
+        for sentence in parse(document_field).split():
+                for token in sentence:
+                    if token[1].startswith('N') or token[1].startswith('J') or token[1].startswith('R'):
+                        doc+=(" "+token[0])
+        return doc
+
+class remove_punctuation(Processer):
+    '''removes all punctuation. "Bla. Bla bla+" -> "Bla Bla bla". "Willem-Alexander" -> WillemAlexander'''
+    try:  #assume python2
+        tbl = dict.fromkeys(i for i in xrange(maxunicode) if unicodedata.category(unichr(i)).startswith('P'))
+    except:   #but do this in python3
+        tbl = dict.fromkeys(i for i in range(maxunicode) if unicodedata.category(chr(i)).startswith('P'))
+
+    def process(self, document_field):
+        '''punctuation removed'''
+         return document_field.replace(u"`",u"").replace(u"´",u"").translate(remove_punctuation.tbl)
+
+class lowercase(Processer):
+    '''converts to lowercase'''
+    def process(self, document_field):
+        '''converted to lowercase'''
+        return document_field.lower()
