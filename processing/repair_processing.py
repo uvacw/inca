aac118bc35a69ef528be4565db085eb11c4ef106
damian0604 <d.c.trilling@uva.nl>
2017-11-09 15:01:57 +0100

    override process instead of run method

--- processing/repair_processing.py
+++ processing/repair_processing.py
@@ -20,49 +20,28 @@ class reparse(Processer):
     by specifiying the new function to parse the content
     '''
 
-    def process(self, document,  source_field, parsefunction):
-        '''reparses {source_field}'''.format(**locals())
-        pass
-
-    def run(self, document, source_field, parsefunction, save=False):
+    def process(self, document_field, parsefunction,**kwargs):
         '''
         Takes a document and a parsing function and re-parses the HTML source.
 
         Parameters
         ---
-        document: dict or string
-            INCA-document that needs to be reparsed.
-            Either a dict with the full document or a string with the document id
-        source_field: string
-            key of the field that contains the HTML source
+        docuent_field: string
+            HTML source to be parsed
+
         parsefunction
             a Python function to parse the HTML source
-        save
-            whether the result should be returned or stored to the database
+
 
         Example
         ---
         p = inca.processing.repair_processing.reparse()
-        p.run('https://www.nu.nl/-/4959386/','htmlsource',inca.scrapers.news_scraper.nu.parsehtml)
+        p.run('https://www.nu.nl/-/4959386/','htmlsource',parsefunction=inca.scrapers.news_scraper.nu.parsehtml, save=True, Force=True)
     
         '''
 
-        if not (type(document) == dict and '_source' in document.keys()):
-
-            if check_exists(document):
-                document = get_document(document)
-            else:
-                logger.debug("document retrieval failure {document}".format(**locals()))
-                return document
-
-        newfields = parsefunction(self, document['_source'][source_field])
-        document['_source'].update(newfields)
-            
-        if save:
-            update_document(document, force=True)
-
-        else:
-            return document
+        newfields = parsefunction(self,htmlsource=document_field)
+        return newfields
 
 class redownload(Processer):
     '''
@@ -71,46 +50,23 @@ class redownload(Processer):
     by specifiying the new function to download the content
     '''
 
-    def process(self, document,  url_field, parsefunction):
-        '''re-downloads {url_field}'''.format(**locals())
-        pass
 
-    def run(self, document, url_field, html_field, downloadfunction, save=False):
+    def process(self, document_field, downloadfunction, **kwargs):
         '''
         Takes a document and a parsing function and re-parses the HTML source.
 
         Parameters
         ---
-        document: dict or string
-            INCA-document that needs to be reparsed.
-            Either a dict with the full document or a string with the document id
-        url_field: string
-            key of the field that contains the URL
-        html_field: string
-            key of the field in which to store the downloaded content 
+        docment_field: string
+            URL to be re-downloaded
         downloadfunction
             a Python function to download the URL
-        save
-            whether the result should be returned or stored to the database
 
         Example
         ---
         p = inca.processing.repair_processing.reparse()
-        p.run('https://www.nu.nl/-/4959386/','url','htmlsource',inca.scrapers.news_scraper.nu.get_page_body)
+        p.run('https://www.nu.nl/-/4959386/','url',downloadfunction=inca.scrapers.news_scraper.nu.get_page_body)
     
         '''
-
-        if not (type(document) == dict and '_source' in document.keys()):
-
-            if check_exists(document):
-                document = get_document(document)
-            else:
-                logger.debug("document retrieval failure {document}".format(**locals()))
-                return document
-
-        document['_source'][html_field] = downloadfunction(self, document['_source'][url_field])  
-        if save:
-            update_document(document, force=True)
-
-        else:
-            return document
+        newdownload = downloadfunction(self, document_field)        
+        return newdownload
