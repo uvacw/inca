2b5f4fd164443ad1fbe8999e777da58c3ca35e55
Chamoetal <chamoetalzeidler@yahoo.com>
2017-12-15 17:34:48 +0100

    added everything needed, still must be tested but i get blocked now

--- scrapers/voedingforum_scraper.py
+++ scrapers/voedingforum_scraper.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
 
 
-# Multipage non-rss scraper. Wanneer een niet-bestaande URL wordt opgevraagd wordt een foutmelding weergeven. De loop stopt zodra deze foutmelding gevonden wordt.
+# Multipage non-rss scraper. When a non-existing URL is requested we get an error messages. The loop stops when the error message is encountered.
 class voedingsforum(Scraper):
     """Scrapes Voedingsforum"""
 
@@ -32,18 +32,7 @@ class voedingsforum(Scraper):
         '''
         self.doctype = "voedingsforum (health)"
         self.version = ".1"
-        self.date = datetime.datetime(year=2017, month=12, day=8)
-
-        posts = []
-        posts_temp = []
-        username_temp = []
-        username = []
-        level_temp = []
-        level = []
-        country_temp = []
-        country = []
-        amount_temp = []
-        amount = []
+        self.date = datetime.datetime(year=2017, month=12, day=15)
 
         allfora = []
         
@@ -57,36 +46,42 @@ class voedingsforum(Scraper):
         linkobjects = tree.xpath('//table//b/a')
         links = [self.BASE_URL+l.attrib['href'] for l in linkobjects if 'href' in l.attrib]
         links = links[0:self.MAXFORA]
-        logger.debug("Er zijn {} links".format(len(links)))
+        logger.debug("There are {} links".format(len(links)))
         logger.debug("\n".join(links))
+        it = 0
         for link in links:
             currentforum = {}
             currentforum['url'] = link
-            # TODO: naam van forum scrapen
-            # currentforum['name'] = 'Afvallen en aankomen' (of zoiets, met XPATH scrapen)
+            treetitles = tree.xpath('//table//b/a/@title')
+            forumtitle = treetitles[it]
+            currentforum['name'] = forumtitle
             currentforum['threads'] = []
             page = 1
             current_page = link
             sleep(randrange(5,10))
             overview_pagesub = requests.get(current_page, cookies = {'cookieAccept' : 'True'})
-# if we want to save the page for test purposes:
-             #with open('/home/chamoetal/Desktop/voedingtest.html', mode = 'w') as fo:
-                 #fo.write(overview_page.text)
-            logger.debug('ik ga nu {} ophalen'.format(link))
-# De loop wordt gestopt door te checken of de pagina de tekst hieronder bevat.
+            logger.debug('now fetching {}'.format(link))
+# The loop stops by checking whether the next page contains the following text.
             while overview_pagesub.content.find(b'Foutmelding' and b'Geen onderwerpen gevonden') == -1:
-                logger.debug('de huidige pagina bevat geen foutmelding')
+                logger.debug('this page does not contain error messages')
                 tree_sub = fromstring(overview_pagesub.text)
                 sublinkobjects = tree_sub.xpath('//table//table//tr/td/a[not(@onmouseout)]/@href')
                 logger.debug('this page has {} sublinkobjects'.format(len(sublinkobjects)))
                 sublinks  = [self.BASE_URL+ li for li in sublinkobjects]
                 logger.debug("\n".join(sublinks))
+                tt = 0
                 for sublink in sublinks:
                     currentthread = {}
+                    treethreadtitles = tree_sub.xpath('//td[@class="forumcellcolor"]//a/text()')
+                    threadtitles = [t for t in treethreadtitles if t.isdigit() == False]
+                    threadtitle = threadtitles[tt]
+                    currentthread['titlethread'] = threadtitle
+                    treethreadread = tree_sub.xpath('//td[@class="forumdark"]/font[@size="2"]/node()')
+                    threadreadclean = [int(t) for t in treethreadread if int(t)>1]
+                    currentthread['timesread'] = threadreadclean[tt]
                     currentthread['posts'] = []
-                    # TODO: we willen ook nog een aantal keys erbij, met name titel en aantal keer gelezen
                     pagesub = 1
-                    logger.debug('ik ga nu {} ophalen'.format(sublink))
+                    logger.debug('fetching now {}'.format(sublink))
                     sleep(randrange(5,10))
                     this_page = requests.get(sublink)
                     while True:
@@ -94,10 +89,10 @@ class voedingsforum(Scraper):
                         thread = treesub.xpath('//table//span[@id="msg"]')
                         messages = [t.text_content() for t in thread]
                         messages = [b.strip() for b in messages]
-                        postlijst = []
+                        postlist = []
                         for message in messages:
                             if len(message)>0:
-                                postlijst.append({'post':message})
+                                postlist.append({'post':message})
 #User information - extracting from the elements. Username and level come together in one list, country of origin and amount of messages in a second list
 
                         user_elem = treesub.xpath('//*[@class="topiclight" or @class="topicdark"]')
@@ -106,22 +101,19 @@ class voedingsforum(Scraper):
                         for user in user_elem:
                             userinfo = user.getchildren()
                             if len(userinfo) == 2:
-                                userleveltemp = userinfo[0].text_content().replace("\r\n", " ") .replace("                 ", " ") .replace("  ", "")
-                                # username_temp.append(userleveltemp.split(" ")[0])
-                                # TODO: de volgende regel ook voor land etc
-                                postlijst[ii]['username'] = userleveltemp.split(" ")[0]
-                                postlijst[ii]['userlevel'] = userleveltemp.split (" ")[1]
-                                # level_temp.append(userleveltemp.split (" ")[1])
+                                userlevel = userinfo[0].text_content()
+                                postlist[ii]['username'] = userlevel.split()[0]
+                                postlist[ii]['userlevel'] = userlevel.split()[1]
                                                             
-                                countryamounttemp = userinfo[1].text_content().replace ("Berichten", " ") .replace("\r\n", " ") .replace ("\t\t", "") .replace("                  ", " ") .replace ("   ", " ")
+                                countryamount = userinfo[1].text_content().replace("Berichten", " ").replace("\r\n", " ").replace("\t\t", "").replace("                  ", " ").replace("   ", " ")
 
-                                thiscountrystring = "".join(re.findall(r"[A-Za-z]", countryamounttemp))
+                                thiscountrystring = "".join(re.findall(r"[A-Za-z]", countryamount))
                                 if thiscountrystring == "":
-                                    postlijst[ii]['usercountry'] ='NA'
+                                    postlist[ii]['usercountry'] ='NA'
                                 else:
-                                    postlijst[ii]['usercountry'] = thiscountrystring
+                                    postlist[ii]['usercountry'] = thiscountrystring
 
-                                postlijst[ii]['useramountofposts'] = int("".join(re.findall(r"[0-9]", countryamounttemp)))
+                                postlist[ii]['useramountofposts'] = int("".join(re.findall(r"[0-9]", countryamount)))
            
                             else:
                                 continue
@@ -134,31 +126,26 @@ class voedingsforum(Scraper):
                         next_url = sublink+'&whichpage='+str(pagesub)
                         sleep(randrange(5,10))
                         next_page = requests.get(next_url)
-                        logger.debug('ik ga nu {} ophalen'.format(next_url))
+                        logger.debug('fetching now {}'.format(next_url))
                         if next_page.content.find(b'/_lib/img/icon_user_profile')==-1:
-                            logger.debug('pagina bestaat niet; ik stop ermee')
+                            logger.debug('page does not exist, stopping now')
                             break
                         else:
                             this_page = next_page
-                    currentthread['posts'] = postlijst
-                    #currentthread['messages'] = post
-                    #currentthread['users'] = country_temp
-                    # TODO: OOK NOG REST
-                    # TODO: dicts in plaats van lange lijsten
-                    # dus niet een lijst met lengte 10 van alle posts en nog eentje met alle users
-                    # maar slechts EEN lijst van dicts, dus zoiets:
-                    # [{'message':'bla bla', 'user':'damian', country:'nl'}, {'message':'nog eentje' ...
+                    currentthread['posts'] = postlist
 
                     currentforum['threads'].append(currentthread)
+                    tt+=1
                 page+=1
                 if page > self.MAXPAGES:
                     break
                 current_page = link+'&sortfield=lastpost&sortorder=desc&whichpage='+str(page)
                 sleep(randrange(5,10))
                 overview_pagesub = requests.get(current_page)
-                logger.debug('ik ga nu {} ophalen'.format(current_page))
-          
+                logger.debug('fetching now {}'.format(current_page))
+                
                 
             allfora.append(currentforum)
+            it+=1
             
         return allfora
