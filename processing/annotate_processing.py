d2bec518610681ba0dfc763e62ff11bee424456a
damian0604 <d.c.trilling@uva.nl>
2018-04-25 14:55:46 +0200

    make sure text is only displayed once if multiple questions are annotated

--- processing/annotate_processing.py
+++ processing/annotate_processing.py
@@ -12,34 +12,34 @@ logger = logging.getLogger(__name__)
 
 WINDOW=150
 
-def _annotate(text, key, question, highlight_regexp):
+def _annotate(text, key, question, highlight_regexp, display=True):
     '''prompts for annotation'''
-    print('\n',Fore.RED,'Please annotate the following text:')
-    if highlight_regexp == None:
-        print(Style.RESET_ALL,text)
-    else:
-        print(Fore.RED+'(only excerpts are shown)'+Style.RESET_ALL)
-        reststring = text
-        numberofmatches=len(re.findall(highlight_regexp,text))
-        poscount=0
-        for i in range(numberofmatches):
-            #print('Occurance {} of {}'.format(i+1,numberofmatches))
-            r=re.search(highlight_regexp,reststring)
-            print(poscount)
-            if poscount+r.start() < WINDOW:
-                print(text[poscount:poscount+r.start()], end='')
-                print(Fore.GREEN+text[poscount+r.start():poscount+r.end()], end='')
-                print(Style.RESET_ALL + text[poscount+r.end():poscount+r.end()+150])
-            else:
-                print(text[poscount+r.start()-WINDOW:poscount+r.start()], end='')
-                print(Fore.GREEN+text[poscount+r.start():poscount+r.end()], end='')
-                print(Style.RESET_ALL + text[poscount+r.end():poscount+r.end()+WINDOW])
-            reststring=reststring[r.end():]
-            poscount+=r.end()
-
-        answer=input(Fore.RED + question + Style.RESET_ALL)
-
-        return answer
+    if display == True:
+        print('\n',Fore.RED,'Please annotate the following text:')
+        if highlight_regexp == None:
+            print(Style.RESET_ALL,text)
+        else:
+            print(Fore.RED+'(only excerpts are shown)'+Style.RESET_ALL)
+            reststring = text
+            numberofmatches=len(re.findall(highlight_regexp,text))
+            poscount=0
+            for i in range(numberofmatches):
+                #print('Occurance {} of {}'.format(i+1,numberofmatches))
+                r=re.search(highlight_regexp,reststring)
+                print(poscount)
+                if poscount+r.start() < WINDOW:
+                    print(text[poscount:poscount+r.start()], end='')
+                    print(Fore.GREEN+text[poscount+r.start():poscount+r.end()], end='')
+                    print(Style.RESET_ALL + text[poscount+r.end():poscount+r.end()+150])
+                else:
+                    print(text[poscount+r.start()-WINDOW:poscount+r.start()], end='')
+                    print(Fore.GREEN+text[poscount+r.start():poscount+r.end()], end='')
+                    print(Style.RESET_ALL + text[poscount+r.end():poscount+r.end()+WINDOW])
+                reststring=reststring[r.end():]
+                poscount+=r.end()
+    
+    answer=input(Fore.RED + question + Style.RESET_ALL)
+    return answer
 
 
 class annotate(Processer):
@@ -50,8 +50,10 @@ class annotate(Processer):
 
         while True:
             annotations = {}
+            display=True
             for key, question in questions.items():
-                annotations[key] = _annotate(document_field,key,question, highlight_regexp)
+                annotations[key] = _annotate(document_field,key,question, highlight_regexp, display)
+                display=False
             print(Fore.RED+'You annotated this document as follows:'+Style.RESET_ALL)
             print(annotations)
             confirm = input(Fore.RED+'\nIs this correct? [Y/N]')
