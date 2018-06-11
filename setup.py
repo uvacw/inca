c7b41747277657fa36635c506bdd8cd89fbeb057
damian0604 <damian0604@users.noreply.github.com>
2018-06-11 15:06:20 +0200

    Make pip install install requirements as well (#376)
    * added required packages to setup.py
    
    * Update README.md

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+requirements = ['celery','colorama','spacy','tqdm','elasticsearch==5.4',
+                'feedparser', 'httplib2','imagehash', 'loremipsum',
+                'lxml','requests','statsmodels','pandas',
+                'pillow==5.0','gensim','oauth2client','matplotlib',
+                'scikit-learn','scipy','selenium','twython','nltk']
+
 setup(name='inca',
       version='0.1',
       description='INfrastructure for Content Analysis',
@@ -10,4 +16,5 @@ setup(name='inca',
       packages=find_packages(),
       package_data={'':['*.cfg']},
       include_package_data=True,
+      install_requires=requirements,
       zip_safe=False)
