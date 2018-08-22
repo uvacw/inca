dea5c33e1e4b09a9fe7e5b9a2fa0c0d7bc7a0b26
damian0604 <d.c.trilling@uva.nl>
2018-08-22 17:08:25 +0200

    created a tagger that returns a bool depending on whether a regexp is found

---
+++ inca/processing/regextagger_processing.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from ..core.processor_class import Processer
+import logging
+import re
+
+
+logger = logging.getLogger("INCA")
+
+
+class regex_tagger(Processer):
+    '''Creates a tag that is either True or False, depending on whether regex is found
+    '''
+    def process(self, document_field, regex, **kwargs):
+        '''is regex mentioned?'''
+
+        r = re.compile(regex)
+
+        if r.findall(document_field):
+            return True
+        else:
+            return False
